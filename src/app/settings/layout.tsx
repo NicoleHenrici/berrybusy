4637be858d2d09d10b80f51f085ae71f6939c169
Nicole <nicole@MacBookPro.fritz.box>
2025-07-11 11:53:50 +0200

    added layout to settings route

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,21 @@
+import SideNav from "@/component/sideNav";
+
+export default function SettingsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex flex-row h-screen">
+      <SideNav
+        navElements={[
+          { href: "/settings/general", label: "General" },
+        ]}
+      />
+
+      <div className="basis-5/6 p-4">
+        {children}
+      </div>
+    </main>
+  );
+}
