8139e8b235710c1c165b7c51a5f2a5b526002fdb
Nicole <nicole@MacBookPro.fritz.box>
2025-07-11 15:06:06 +0200

    (settings) integrate Card component

--- src/app/settings/general/page.tsx
+++ src/app/settings/general/page.tsx
@@ -1,8 +1,31 @@
+import Card from "@/component/card";
+
+const settingsSections = [
+  {
+    type: "checkbox",
+    label: "Dark Mode",
+    optionLabel: ["Enable Dark Mode"],
+  },
+  {
+    type: "checkbox",
+    label: "Color Scheme",
+    optionLabel: ["strawberry", "blueberry"],
+  },
+  {
+    type: "number",
+    label: "Font Size",
+    optionLabel: ["Small", "Medium", "Large"],
+  },
+];
+
+
 export default function SettingsGeneralPage() {
   return (
     <>
-      <h1>General Settings</h1>
-      <p>This is the general settings page.</p>
+      <h2 className="text-lg font-bold mb-2">General Settings</h2>
+      <Card
+        sections={settingsSections}
+      />
     </>
   );
 }
