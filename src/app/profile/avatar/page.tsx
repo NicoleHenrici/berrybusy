4340e1a368afec848ae0b4097a3fd9913b5ab57a
Nicole <nicole@MacBookPro.fritz.box>
2025-07-11 11:52:53 +0200

    added subpages to profile route

---
+++ src/app/profile/avatar/page.tsx
@@ -0,0 +1,8 @@
+export default function AvatarPage() {
+  return (
+    <>
+      <h1>Avatar</h1>
+      <p>This is the avatar page.</p>
+    </>
+  );
+}
---
+++ src/app/profile/userData/page.tsx
@@ -0,0 +1,8 @@
+export default function UserDataPage() {
+  return (
+    <>
+      <h1>User Data</h1>
+      <p>This is the user data page.</p>
+    </>
+  );
+}
