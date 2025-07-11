59a432fd5056f129d973d664b2e348e14234fce6
Nicole <nicole@MacBookPro.fritz.box>
2025-07-11 11:53:18 +0200

    added layout to profile route

---
+++ src/app/profile/layout.tsx
@@ -0,0 +1,22 @@
+import SideNav from "@/component/sideNav";
+
+export default function ProfileLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main className="flex flex-row h-screen">
+      <SideNav
+        navElements={[
+          { href: "/profile/userData", label: "User Data" },
+          { href: "/profile/avatar", label: "Avatar" },
+        ]}
+      />
+
+      <div className="basis-5/6 p-4">
+        {children}
+      </div>
+    </main>
+  );
+}
