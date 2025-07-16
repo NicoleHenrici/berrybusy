4ad8a9e1515dce2d8006ef8fe3e056614278e3af
Nicole <nicole@MacBookPro.fritz.box>
2025-07-16 11:21:05 +0200

    profile/userData: adjust passed prop for Card component, implement submit button in form

--- src/app/profile/userData/page.tsx
+++ src/app/profile/userData/page.tsx
@@ -3,23 +3,31 @@ import Card from "@/component/card";
 const settingsSections = [
   {
     type: "text",
-    label: "Name",
-    optionLabel: [],
+    sectionLabel: "Name",
+    options: [],
   },
   {
     type: "text-group",
-    label: "Address Information",
-    optionLabel: ["phone", "street", "city", "zip"],
+    sectionLabel: "Address Information",
+    options: [
+      { label: "phone", placeholder: "Enter your phone number", value: "" },
+      { label: "street", placeholder: "Enter your street address", value: "" },
+      { label: "city", placeholder: "Enter your city", value: "" },
+      { label: "zip", placeholder: "Enter your zip code", value: "" },
+    ],
   },
 ];
 
 export default function UserDataPage() {
   return (
-   <>
-         <Card
-           sections={settingsSections}
-           pageName="User Data Settings"
-         />
-       </>
+    <>
+      <form className="border border-foreground/25 rounded-md m-auto mt-0 w-fit flex flex-col items-end">
+        <Card
+          sections={settingsSections}
+          pageName="User Data Settings"
+        />
+        <button type="submit" className="btn m-2 bg-brand-primary p-2 text-white rounded-md">Save Changes</button>
+      </form>
+    </>
   );
 }
