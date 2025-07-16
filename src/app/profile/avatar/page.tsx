ad2bdb3c067283ecf4dade3ab87db06940e92327
Nicole <nicole@MacBookPro.fritz.box>
2025-07-16 11:16:52 +0200

    profile/avatar: integrate Card component

--- src/app/profile/avatar/page.tsx
+++ src/app/profile/avatar/page.tsx
@@ -1,8 +1,36 @@
+import Card from "@/component/card";
+import { FiUpload } from "react-icons/fi";
+
+const settingsSections = [
+  {
+    type: "image",
+    sectionLabel: "",
+    options: [
+      // value will contain path of active avatar
+      { label: "Choosen Avatar", placeholder: "Choose an image file", value: "" },
+    ],
+  },
+  {
+    type: "upload",
+    sectionLabel: "Upload Avatar  ",
+    options: [
+      { label: "Upload Image", placeholder: "Choose an image file", value: "" },
+    ],
+  },
+];
+
 export default function AvatarPage() {
   return (
     <>
-      <h1>Avatar</h1>
-      <p>This is the avatar page.</p>
+      <form className="border border-foreground/25 rounded-md m-auto mt-0 w-fit flex flex-col items-end">
+        <Card
+          sections={settingsSections}
+          pageName="Avatar"
+        />
+        <button type="submit" className="btn m-2 bg-brand-primary p-2 text-white rounded-md">
+          <span className="inline-block align-text-top"><FiUpload /></span> Upload
+        </button>
+      </form>
     </>
   );
 }
