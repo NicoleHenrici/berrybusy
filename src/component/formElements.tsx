9cbc91b4f80b0a812218fc4492642b9962906621
Nicole <nicole@MacBookPro.fritz.box>
2025-08-13 11:58:53 +0200

    feature: extend card with separate child chomponent for form

---
+++ src/component/formElements.tsx
@@ -0,0 +1,12 @@
+export default function FormElements() {
+  return (
+    <>
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Label</span>
+        </label>
+        <input type="text" className="input" />
+      </div>
+    </>
+  );
+}
