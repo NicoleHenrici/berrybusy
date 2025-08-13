92e10eab30eb6a338b18fa3519dc99dccd27efd7
Nicole <nicole@MacBookPro.fritz.box>
2025-08-13 11:23:55 +0200

    refactor: move lib/ into src path

--- src/lib/recipes.ts
+++ src/lib/recipes.ts
@@ -2,7 +2,7 @@ import sql from 'better-sqlite3';
 
 const db = new sql('recipes.db');
 
-export function getRecipes() {
+export default function getRecipes() {
     const stmt = db.prepare('SELECT * FROM recipes');
     return stmt.all();
 }
