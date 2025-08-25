6d02a00fba88bd1dafbdbf0f96e15e8d0a313f14
Nicole <nicole@MacBookPro.fritz.box>
2025-08-25 11:48:37 +0200

    feature: add card component to recipes overview for recipe display

--- src/app/recipes/recipesOverview/page.tsx
+++ src/app/recipes/recipesOverview/page.tsx
@@ -1,13 +1,28 @@
 import getRecipes from "@/lib/recipes";
 
+type Recipe = {
+    slug: string | number;
+    name: string;
+    duration: number;
+    ingredients: string;
+    instructions: string;
+    image: string;
+};
+
 export default async function RecipesOverview() {
-    const recipes = await getRecipes();
+    const recipes = await getRecipes() as Recipe[];
 
     console.log(recipes)
     return (
         <>
             <h1>Overview</h1>
             <p>This is the recipesoverview page.</p>
+            {recipes.map((recipe) => (
+              <div key={recipe.slug}>
+                <h2>{recipe.name}</h2>
+                <p>{recipe.ingredients}</p>
+              </div>
+            ))}
         </>
     );
 }
